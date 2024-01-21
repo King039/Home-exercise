1f374485b67b4aa9e11e03b2aba13c5f336d3c02
King039 <bazanks039@gmail.com>
2024-01-21 17:52:53 +0200

    Добавьте файлы проекта.

---
+++ Home exercise/Program.cs
@@ -0,0 +1,18 @@
+﻿string myString = "28";
+Console.WriteLine(myString);
+
+byte myByte = byte.Parse(myString);
+Console.WriteLine(myByte);
+
+nint myNint = nint.Parse(myString);
+Console.WriteLine(myNint);
+
+int myInt = int.Parse(myString);
+Console.WriteLine(myInt);
+
+long myLong = long.Parse(myString);
+Console.WriteLine(myLong);
+
+double myDouble = double.Parse(myString);
+Console.WriteLine(myDouble);
+
