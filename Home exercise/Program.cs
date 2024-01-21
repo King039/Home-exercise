ca4b635e4c04ef7343eab91b2355d502e8cf73a9
King039 <bazanks039@gmail.com>
2024-01-21 18:18:15 +0200

    dz 7.1

--- Home exercise/Program.cs
+++ Home exercise/Program.cs
@@ -1,18 +1,29 @@
-﻿string myString = "28";
-Console.WriteLine(myString);
+﻿//string myString = "28";
+//Console.WriteLine(myString);
 
-byte myByte = byte.Parse(myString);
-Console.WriteLine(myByte);
+//byte myByte = byte.Parse(myString);
+//Console.WriteLine(myByte);
 
-nint myNint = nint.Parse(myString);
-Console.WriteLine(myNint);
+//nint myNint = nint.Parse(myString);
+//Console.WriteLine(myNint);
 
-int myInt = int.Parse(myString);
-Console.WriteLine(myInt);
+//int myInt = int.Parse(myString);
+//Console.WriteLine(myInt);
 
-long myLong = long.Parse(myString);
-Console.WriteLine(myLong);
+//long myLong = long.Parse(myString);
+//Console.WriteLine(myLong);
 
-double myDouble = double.Parse(myString);
-Console.WriteLine(myDouble);
+//double myDouble = double.Parse(myString);
+//Console.WriteLine(myDouble);
+
+
+
+double PI = 3.14;
+Console.WriteLine("Введите радиус окружности: ");
+double radius = double.Parse(Console.ReadLine());
+Console.WriteLine("Площадь круга равна: ");
+Console.WriteLine(PI *  radius  *  radius);
+
+
+Console.WriteLine()
 
