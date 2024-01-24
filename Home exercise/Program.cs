35f4a4c6bbc92a8cde0300c6128044d01fa2d992
King039 <bazanks039@gmail.com>
2024-01-24 18:34:30 +0200

    дз 5.2

--- Home exercise/Program.cs
+++ Home exercise/Program.cs
@@ -85,7 +85,8 @@
 
 // дз 5.2
 
-// Первый продукт
+// Первый продукт "кофе стандарт" цена 69,0 ; колличество 1
+
 
 Console.WriteLine("Укажите продукт: ");
 string firstProductName = Console.ReadLine();
@@ -99,7 +100,7 @@ int firstProductQuantity = int.Parse(Console.ReadLine());
 decimal sum = firstProductPrice * firstProductQuantity;
 
 
-//// Второй продукт
+//// Второй продукт "рулет-спринг тв из ку" цена 70,0 ; колличество 1
 
 Console.WriteLine("Укажите продукт: ");
 string secondProductName = Console.ReadLine();
@@ -113,14 +114,47 @@ int secondProductQuantity = int.Parse(Console.ReadLine());
 decimal sum2 = secondProductPrice * secondProductQuantity;
 
 
-Console.WriteLine("№\tНазвание\tЦена\tКолличество\tСумма");
 
-Console.WriteLine($"1\t{firstProductName}\t{firstProductPrice}\t{firstProductQuantity}\t{sum}");
+Console.WriteLine("*********************************************************");
 
-Console.WriteLine("НДС с расчитаной ставкой 18%\t\t\t10.53");
+Console.WriteLine("№ Название\t\t\tЦена\tКолличество\tСумма");
 
-Console.WriteLine($"2\t{secondProductName}\t{secondProductPrice}\t{secondProductQuantity}\t{sum2}");
+Console.WriteLine($"№1 {firstProductName}\t\t{firstProductPrice}\t{firstProductQuantity}\t\t{sum}");
 
-Console.WriteLine("НДС с расчитаной ставкой 10%\t\t\t6.36");
+Console.WriteLine("НДС с расчитаной ставкой 18%\t\t\t\t10.53");
+
+Console.WriteLine($"№2 {secondProductName}\t{secondProductPrice}\t{secondProductQuantity}\t\t{sum2}");
+
+Console.WriteLine("НДС с расчитаной ставкой 10%\t\t\t\t6.36");
+
+Console.WriteLine("*********************************************************");
 
 Console.WriteLine($"Итого: {sum + sum2}");
+
+Console.WriteLine(" ");
+
+Console.WriteLine(" ");
+
+Console.WriteLine($"Электронные средства:\t\t\t\t{sum + sum2}");
+
+Console.WriteLine("НДС итога чека с расчитаной ставкой\n18%\t10.53");
+
+Console.WriteLine("НДС итога чека с расчитаной ставкой\n10%\t6.36");
+
+Console.WriteLine(" ");
+
+Console.WriteLine(" ");
+
+Console.WriteLine("*********************************************************");
+
+Console.WriteLine("ВИД НАЛОГООБЛАЖЕНИЯ: ОСН");
+
+Console.WriteLine("РЕГ. НОМЕР ККТ: 0000143873023483");
+
+Console.WriteLine("ЗАВОД. №:");
+
+Console.WriteLine("ФН. №: 8710000100265168");
+
+Console.WriteLine("ФД. №: 6026");
+
+Console.WriteLine("ФПД: 3322278314");
